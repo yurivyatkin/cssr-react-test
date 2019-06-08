4b8712bd78f704b017ae0abbe1f989402a955dee
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2019-06-08 16:23:49 +1200

    Fix the timer- use setInterval;- milliseconds times 1000;- clear the timeout.

--- src/index.js
+++ src/index.js
@@ -137,19 +137,23 @@ class TimerComponent extends React.Component {
     );
   }
 
+  timeout = null;
+
   handleStart = () => {
-    setTimeout(
+    this.timeout = setInterval(
       () =>
         this.setState({
           currentTime: this.state.currentTime + this.props.currentInterval
         }),
-      this.props.currentInterval
+      this.props.currentInterval * 1000
     );
-  }
+  };
 
   handleStop = () => {
-    this.setState({ currentTime: 0 });
-  }
+    this.setState({ currentTime: 0 }, () => {
+      clearTimeout(this.timeout);
+    });
+  };
 }
 
 const Timer = connect(
