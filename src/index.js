088dd0717ec5d96157298b90de5ff45fffd64b2b
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2019-06-08 14:47:40 +1200

    Prettify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 // Slomux — упрощённая, сломанная реализация Flux.
 // Перед вами небольшое приложение, написанное на React + Slomux.
@@ -12,150 +12,154 @@ import PropTypes from 'prop-types';
 // При нажатии на "стоп" секундомер должен останавливаться и сбрасывать свое значение
 
 const createStore = (reducer, initialState) => {
-    let currentState = initialState
-    const listeners = []
-  
-    const getState = () => currentState
-    const dispatch = action => {
-      currentState = reducer(currentState, action)
-      listeners.forEach(listener => listener())
-    }
-  
-    const subscribe = listener => listeners.push(listener)
-  
-    return { getState, dispatch, subscribe }
-  }
-  
-  const connect = (mapStateToProps, mapDispatchToProps) =>
-    Component => {
-      class WrappedComponent extends React.Component {
-        render() {
-          return (
-            <Component
-              {...this.props}
-              {...mapStateToProps(this.context.store.getState(), this.props)}
-              {...mapDispatchToProps(this.context.store.dispatch, this.props)}
-            />
-          )
-        }
-  
-        componentDidUpdate() {
-          this.context.store.subscribe(this.handleChange)
-        }
-  
-        handleChange = () => {
-          this.forceUpdate()
-        }
-      }
-  
-      WrappedComponent.contextTypes = {
-        store: PropTypes.object,
-      }
-  
-      return WrappedComponent
-    }
-  
-  class Provider extends React.Component {
-    getChildContext() {
-      return {
-        store: this.props.store,
-      }
-    }
-    
-    render() {
-      return React.Children.only(this.props.children)
-    }
-  }
-  
-  Provider.childContextTypes = {
-    store: PropTypes.object,
-  }
-  
-  // APP
-  
-  // actions
-  const CHANGE_INTERVAL = 'CHANGE_INTERVAL'
-  
-  // action creators
-  const changeInterval = value => ({
-    type: CHANGE_INTERVAL,
-    payload: value,
-  })
-  
-  
-  // reducers
-  const reducer = (state, action) => {
-    switch(action.type) {
-      case CHANGE_INTERVAL:
-        return state += action.payload
-      default:
-        return {}
-    }
-  }
-  
-  // components
-  
-  class IntervalComponent extends React.Component {
+  let currentState = initialState;
+  const listeners = [];
+  const getState = () => currentState;
+  const dispatch = action => {
+    currentState = reducer(currentState, action);
+    listeners.forEach(listener => listener());
+  };
+  const subscribe = listener => listeners.push(listener);
+
+  return { getState, dispatch, subscribe };
+};
+
+const connect = (mapStateToProps, mapDispatchToProps) => Component => {
+  class WrappedComponent extends React.Component {
     render() {
       return (
-        <div>
-          <span>Интервал обновления секундомера: {this.props.currentInterval} сек.</span>
-          <span>
-            <button onClick={() => this.props.changeInterval(-1)}>-</button>
-            <button onClick={() => this.props.changeInterval(1)}>+</button>
-          </span>
-        </div>
-      )
+        <Component
+          {...this.props}
+          {...mapStateToProps(this.context.store.getState(), this.props)}
+          {...mapDispatchToProps(this.context.store.dispatch, this.props)}
+        />
+      );
     }
+
+    componentDidUpdate() {
+      this.context.store.subscribe(this.handleChange);
+    }
+
+    handleChange = () => {
+      this.forceUpdate();
+    };
   }
-  
-  const Interval = connect(dispatch => ({
-    changeInterval: value => dispatch(changeInterval(value)),
+
+  WrappedComponent.contextTypes = {
+    store: PropTypes.object
+  };
+
+  return WrappedComponent;
+};
+
+class Provider extends React.Component {
+  getChildContext() {
+    return {
+      store: this.props.store
+    };
+  }
+
+  render() {
+    return React.Children.only(this.props.children);
+  }
+}
+
+Provider.childContextTypes = {
+  store: PropTypes.object
+};
+
+// APP
+
+// actions
+const CHANGE_INTERVAL = "CHANGE_INTERVAL";
+
+// action creators
+const changeInterval = value => ({
+  type: CHANGE_INTERVAL,
+  payload: value
+});
+
+// reducers
+const reducer = (state, action) => {
+  switch (action.type) {
+    case CHANGE_INTERVAL:
+      return (state += action.payload);
+    default:
+      return {};
+  }
+};
+
+// components
+
+class IntervalComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>
+          Интервал обновления секундомера: {this.props.currentInterval} сек.
+        </span>
+        <span>
+          <button onClick={() => this.props.changeInterval(-1)}>-</button>
+          <button onClick={() => this.props.changeInterval(1)}>+</button>
+        </span>
+      </div>
+    );
+  }
+}
+
+const Interval = connect(
+  dispatch => ({
+    changeInterval: value => dispatch(changeInterval(value))
   }),
   state => ({
-    currentInterval: state,
-  }))(IntervalComponent)
-  
-  class TimerComponent extends React.Component {
-    state = {
-      currentTime: 0
-    }
-  
-    render() {
-      return (
+    currentInterval: state
+  })
+)(IntervalComponent);
+
+class TimerComponent extends React.Component {
+  state = {
+    currentTime: 0
+  };
+
+  render() {
+    return (
+      <div>
+        <Interval />
+        <div>Секундомер: {this.state.currentTime} сек.</div>
         <div>
-          <Interval />
-          <div>
-            Секундомер: {this.state.currentTime} сек.
-          </div>
-          <div>
-            <button onClick={this.handleStart}>Старт</button>
-            <button onClick={this.handleStop}>Стоп</button>
-          </div>
+          <button onClick={this.handleStart}>Старт</button>
+          <button onClick={this.handleStop}>Стоп</button>
         </div>
-      )
-    }
-  
-    handleStart() {
-      setTimeout(() => this.setState({
-        currentTime: this.state.currentTime + this.props.currentInterval,
-      }), this.props.currentInterval)
-    }
-    
-    handleStop() {
-      this.setState({ currentTime: 0 })
-    }
+      </div>
+    );
   }
-  
-  const Timer = connect(state => ({
-    currentInterval: state,
-  }), () => {})(TimerComponent)
-  
-  // init
-  ReactDOM.render(
-    <Provider store={createStore(reducer)}>
-      <Timer />
-    </Provider>,
-    document.getElementById('app')
-  )
-  
+
+  handleStart() {
+    setTimeout(
+      () =>
+        this.setState({
+          currentTime: this.state.currentTime + this.props.currentInterval
+        }),
+      this.props.currentInterval
+    );
+  }
+
+  handleStop() {
+    this.setState({ currentTime: 0 });
+  }
+}
+
+const Timer = connect(
+  state => ({
+    currentInterval: state
+  }),
+  () => {}
+)(TimerComponent);
+
+// init
+ReactDOM.render(
+  <Provider store={createStore(reducer)}>
+    <Timer />
+  </Provider>,
+  document.getElementById("app")
+);
